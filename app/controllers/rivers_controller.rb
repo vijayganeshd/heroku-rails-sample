db8bfae2c1b2b87d05185bfbba48d30dc5752549
vijayganeshd <30308826+vijayganeshd@users.noreply.github.com>
2018-02-12 14:22:07 +0530

    Update rivers_controller.rb

--- app/controllers/rivers_controller.rb
+++ app/controllers/rivers_controller.rb
@@ -1,6 +1,7 @@
 class RiversController < ApplicationController
   def index
     put "I am in the riverbrain page"
+    put "I am in the riverbrain page"
     render json: RestClient.get("http://riverbrain.com/api/v1/rivers", {})
   end
 end
